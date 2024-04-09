f4f85e84dc75a448c44ae5cb15f253cadf7671a9
Ansh <vaguecoder145@gmail.com>
2024-04-09 23:16:33 +0530

    Migrate project to PostgreSQL

--- src/main/java/com/example/JobApp/model/Job.java
+++ src/main/java/com/example/JobApp/model/Job.java
@@ -1,8 +1,16 @@
 package com.example.JobApp.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Data
 public class Job {
 
     @Id
@@ -14,15 +22,10 @@ public class Job {
     private Integer maxSalary;
     private String location;
 
-//    Many to one mapping added as many jobs can be linked to a single company
+//    Many-to-one mapping added as many jobs can be linked to a single company
 //    While creating job we can just pass id of the company and this mapping will automatically take care of the job to company mapping
     @ManyToOne
     private Company company;
-
-//    this is needed by JPA to instantiate objects using retrieved data
-    public Job() {
-
-    }
 
     public Job(Long id, String title, String destination, Integer minSalary, Integer maxSalary, String location, Company company) {
         this.id = id;
@@ -34,59 +37,4 @@ public class Job {
         this.company = company;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public Integer getMinSalary() {
-        return minSalary;
-    }
-
-    public Integer getMaxSalary() {
-        return maxSalary;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public void setMinSalary(Integer minSalary) {
-        this.minSalary = minSalary;
-    }
-
-    public void setMaxSalary(Integer maxSalary) {
-        this.maxSalary = maxSalary;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Company getCompany() {
-        return company;
-    }
-
-    public void setCompany(Company company) {
-        this.company = company;
-    }
 }
